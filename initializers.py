649585e7b0c6dec8e9ec69a8bf7c844ed38a4f0a
binh <binh.do1120@hcmut.edu.vn>
2022-04-24 18:00:43 +0700

    initializers is now a class

--- initializers.py
+++ initializers.py
@@ -3,40 +3,42 @@ particle initializers \n
 each function returns a (cnn_layers, fc_layers) tuple \n
 this tuple does not include an output layer and a global pooling / flatten layer
 '''
-def sequential(config_file='config.json'):
-    '''
-    add layers to the list of layers, following transition rules as defined in config_file
-    '''
-    import json
-    import os.path
-    import random
+class Initializers:
+    def __init__(self, config_file='config.json'):
+        import json
+        import os.path
+        
+        self.config = {}
+        if os.path.exists(config_file):
+            with open(config_file, 'r') as f:
+                self.config = json.load(f)
 
-    config = {}
-    if os.path.exists(config_file):
-        with open(config_file, 'r') as f:
-            config = json.load(f)
+    def sequential(self):
+        '''
+        creates a sequential model following transition rules
+        '''
+        import random
+        num_cnn_layers = random.randint(self.config['min_cnn_layers'], self.config['max_cnn_layers'])
+        num_fc_layers = random.randint(self.config['min_fc_layers'], self.config['max_fc_layers'])
+        cnn_layers = []
+        fc_layers = []
 
-    num_cnn_layers = random.randint(config['min_cnn_layers'], config['max_cnn_layers'])
-    num_fc_layers = random.randint(config['min_fc_layers'], config['max_fc_layers'])
-    cnn_layers = []
-    fc_layers = []
+        next_layer_type = 'conv'
+        for _ in range(num_cnn_layers):
+            layer = {}
+            layer['type'] = next_layer_type
+            for key in self.config['config'][next_layer_type]:
+                layer[key] = random.choice(self.config['config'][next_layer_type][key])
+            cnn_layers.append(layer)
+            next_layer_type = random.choices(self.config['layers'], self.config['transitions'][next_layer_type])[0]
 
-    next_layer_type = 'conv'
-    for _ in range(num_cnn_layers):
-        layer = {}
-        layer['type'] = next_layer_type
-        for key in config['config'][next_layer_type]:
-            layer[key] = random.choice(config['config'][next_layer_type][key])
-        cnn_layers.append(layer)
-        next_layer_type = random.choices(config['layers'], config['transitions'][next_layer_type])[0]
+        next_layer_type = 'fc'
+        for _ in range(num_fc_layers):
+            layer = {}
+            layer['type'] = next_layer_type
+            for key in self.config['config'][next_layer_type]:
+                layer[key] = random.choice(self.config['config'][next_layer_type][key])
+            fc_layers.append(layer)
+            next_layer_type = random.choices(self.config['layers'], self.config['transitions'][next_layer_type])[0]
 
-    next_layer_type = 'fc'
-    for _ in range(num_fc_layers):
-        layer = {}
-        layer['type'] = next_layer_type
-        for key in config['config'][next_layer_type]:
-            layer[key] = random.choice(config['config'][next_layer_type][key])
-        fc_layers.append(layer)
-        next_layer_type = random.choices(config['layers'], config['transitions'][next_layer_type])[0]
-
-    return cnn_layers, fc_layers
+        return cnn_layers, fc_layers
--- pso_cnn.py
+++ pso_cnn.py
@@ -81,8 +81,8 @@ class Particle:
         self.cnn_layers = [] #both convolution layers and pooling layers
         self.fc_layers = [] #dense layers
 
-    def initialize(self, initializer=initializers.sequential):
-        self.cnn_layers, self.fc_layers = initializer()
+    def initialize(self, initializer=initializers.Initializers()):
+        self.cnn_layers, self.fc_layers = initializer.sequential()
 
     def save(self, filename):
         if not os.path.exists('save'):
